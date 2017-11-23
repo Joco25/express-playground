eb211ae0957f6c4c20e6991a79eabcf46bc121be
joco25 <onungwej@gmail.com>
2017-11-23 05:17:04 +0100

    Modified files for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express =require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
 var app = express()
 
 // Middlewares
@@ -58,6 +59,6 @@ app.get('/bad',(req,res)=>{
 // 	})
 // })
 
-app.listen(3000,()=>{
-	console.log('Server started, running on port 3000')
+app.listen(port,()=>{
+	console.log(`Server started, running on port ${port}`)
 })
